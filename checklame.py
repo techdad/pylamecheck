085a27f22fa9f38a4dfb1fdba4b530905b935800
Daniel Shaw <daniel@afrinic.net>
2018-01-29 16:25:42 +0400

    clean-up, simplify, handle non-dual stack (manually, and json output

--- checklame.py
+++ checklame.py
@@ -4,10 +4,11 @@
 # [ under construction ]
 
 import sys
-#import pprint
+import json
 import getdns
 
-DEBUG_ON = False
+DEBUG_ON = True
+IPV6_YES = True
 
 def main():
     """main function"""
@@ -19,44 +20,64 @@ def main():
     # args are the right sort of strings for what they should be
     # also need to deal with trailing ".", which currently breaks
 
-    is_lame(sys.argv[1], sys.argv[2])
+    result = is_lame(sys.argv[1], sys.argv[2])
+    print result
 
 
 def is_lame(domain_name, nserver_name):
     """lameness check function"""
 
+    out = {'domain': domain_name, 'nserver': nserver_name}
+
     # lookup the nserver's IP address(es)
-    ip_ctx = getdns.Context(set_from_os=1)
-    extensions = {"return_both_v4_and_v6" : getdns.EXTENSION_TRUE}
-    ip_ctx.resolution_type = getdns.RESOLUTION_RECURSING
+    ctx = getdns.Context()
 
     try:
-        nserver_ips = ip_ctx.address(name=nserver_name, extensions=extensions)
+        nserver_ips = ctx.address(name=nserver_name)
     except getdns.error, err:
         print str(err)
         sys.exit(1)
 
     if nserver_ips.status == getdns.RESPSTATUS_GOOD:
-        upstream_ns = nserver_ips.just_address_answers
+        # remove AAAA responses if IPv6 flag is off
+        if IPV6_YES:
+            upstream_ns = nserver_ips.just_address_answers
+        elif not IPV6_YES:
+            answers = nserver_ips.just_address_answers
+            upstream_ns = [ip for ip in answers if ip.get('address_type', '') != 'IPv6']
+        else:
+            print "WTF!"
+            sys.exit(1)
+
+    elif nserver_ips.status == getdns.RESPSTATUS_NO_NAME:
+        out['status'] = 'LAME'
+        out['detail'] = 'cannot resolve nserver name (negative response)'
+        return json.dumps(out)
+
+    elif nserver_ips.status == getdns.RESPSTATUS_ALL_TIMEOUT:
+        out['status'] = 'LAME'
+        out['detail'] = 'cannot resolve nserver name (negative response)'
+        return json.dumps(out)
+
     else:
-        # this needs improving - ns cant be resolved = lame too
-        print "OOPS!: Address lookup of {0} failed. (Error code: {1})."\
+        print "OOPS!: Lookup failure for {0}. (Error code: {1})."\
                 .format(nserver_name, nserver_ips.status)
         sys.exit(1)
 
-    # lookup the domain SOA...
-    soa_ctx = getdns.Context(set_from_os=0)
-    extensions = {"return_call_reporting" : getdns.EXTENSION_TRUE}
-    soa_ctx.resolution_type = getdns.RESOLUTION_STUB
+    # lookup the domain's SOA...
+    ctx = getdns.Context(set_from_os=0)
+    ctx.resolution_type = getdns.RESOLUTION_STUB
 
     # ... trying each IP obtained above, in turn
     for ns_ip in upstream_ns:
-        ns_notlame = False
 
-        soa_ctx.upstream_recursive_servers = [ns_ip]
+        if DEBUG_ON:
+            debug_out = {'DEBUG': {'query': ns_ip}}
+            print json.dumps(debug_out)
+
+        ctx.upstream_recursive_servers = [ns_ip]
         try:
-            results = soa_ctx.general(name=domain_name, \
-                    request_type=getdns.RRTYPE_SOA, extensions=extensions)
+            results = ctx.general(name=domain_name, request_type=getdns.RRTYPE_SOA)
         except getdns.error, err:
             print str(err)
             sys.exit(1)
@@ -64,24 +85,16 @@ def is_lame(domain_name, nserver_name):
         # and check for the AA bit set
         if results.status == getdns.RESPSTATUS_GOOD and \
                                 results.replies_tree[0]['header']['aa'] == 1:
-            ns_notlame = True
+            # one success is enough
+            out['status'] = 'OK'
+            out['detail'] = 'aa'
+            return json.dumps(out)
+        # deal with other repsonse codes here...
 
-        # start debug (remove later)
-        if DEBUG_ON:
-            debug_res_out = [domain_name, results.call_reporting[0]['query_to']['address_data']]
-            if ns_notlame:
-                debug_res_out.append("OK")
-            else:
-                debug_res_out.append("LAME")
-            print "DEBUG: {0} @ {1} = {2}".format(*debug_res_out)
-        # end debug section
-
-    res_out = [domain_name, nserver_name, results.call_reporting[0]['run_time/ms']]
-    if ns_notlame:
-        res_out.append("OK")
-    else:
-        res_out.append("LAME")
-    print "domain: {0}, nserver: {1}, result = {3} ({2}ms)".format(*res_out)
+    # if nothing was successful, then lame
+    out['status'] = 'LAME'
+    out['detail'] = ''
+    return json.dumps(out)
 
 
 if __name__ == "__main__":
