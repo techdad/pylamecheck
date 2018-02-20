2753f6e912462859ad52a6b0892493c24b2c6ca6
Daniel Shaw <daniel@afrinic.net>
2018-02-20 17:30:54 +0400

    for running under supervisor

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+WRKRS=3
+PORTN=7403
+USERN=nobody
+
+cd $(dirname $0)
 source venv/bin/activate
 
-WRKS=3
-PORT=7403
-
-gunicorn -b [::1]:$PORT -b 127.0.0.1:$PORT \
-		-w $WRKS checklame_rest:FLASK_APP
+gunicorn -b [::1]:$PORTN -b 127.0.0.1:$PORTN \
+		-u $USERN -w $WRKRS checklame_rest:FLASK_APP
 
 # eof
