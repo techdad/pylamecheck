10b35f30b86a4e669f9812a897f33368363ce771
Daniel Shaw <daniel@afrinic.net>
2018-04-10 22:01:51 +0400

    bourne shell/posix compliance

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 WRKRS=3
 PORTN=7403
-USERN=nobody
+USERN=daniel
 
 cd $(dirname $0)
-source venv/bin/activate
+. venv/bin/activate
 
 gunicorn -b [::1]:$PORTN -b 127.0.0.1:$PORTN \
 		-u $USERN -w $WRKRS checklame_rest:FLASK_APP
